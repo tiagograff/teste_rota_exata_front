8e1a98e5ce78ffd63734e8571861b0e6b97b9d3b
tiago <tiagopinzgraffunder@gmail.com>
2025-01-22 15:25:28 -0300

    refactor: arrumando visualização de senha

--- public/js/eyes.js
+++ public/js/eyes.js
@@ -1,16 +1,37 @@
 export function eyes(eye, userinput) {
+  const currentPage = window.location.pathname;
+  let primaryURL, secondaryURL;
+
+  if (currentPage.includes("register.html")) {
+    primaryURL = "../img/eye.svg";
+    secondaryURL = "../img/eye-slash-solid.svg";
+  } else {
+    // Definir um valor padrão se necessário
+    primaryURL = "./public/img/eye.svg";
+    secondaryURL = "./public/img/eye-slash-solid.svg";
+  }
+
   eye.addEventListener("click", (event) => {
     event.preventDefault();
+
     if (eye.classList.contains("eyesclose")) {
       eye.classList.remove("eyesclose");
       eye.classList.add("eyesopen");
-      eye.style.backgroundImage = `url("./public/img/eye.svg")`;
+      setBackgroundImage(eye, primaryURL, secondaryURL);
       userinput.type = "text";
     } else if (eye.classList.contains("eyesopen")) {
       eye.classList.remove("eyesopen");
       eye.classList.add("eyesclose");
-      eye.style.backgroundImage = `url("./public/img/eye-slash-solid.svg")`;
+      setBackgroundImage(eye, secondaryURL, primaryURL);
       userinput.type = "password";
     }
   });
+}
+
+function setBackgroundImage(element, primaryURL, secondaryURL) {
+  element.style.backgroundImage = `url("${primaryURL}")`;
+
+  element.onerror = () => {
+    element.style.backgroundImage = `url("${secondaryURL}")`;
+  };
 }
--- public/js/register.js
+++ public/js/register.js
@@ -1,52 +1,62 @@
-let registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || []; // Corrigido o nome da chave
-const registerButton = document.getElementById("registerButton");
-const password = document.getElementById("password");
-const email = document.getElementById("email");
-const username = document.getElementById("username");
-const date = document.getElementById("birthday");
+import { eyes } from "./eyes.js";
+document.addEventListener("DOMContentLoaded", () => {
+  let registeredUsers =
+    JSON.parse(localStorage.getItem("registeredUsers")) || []; // Corrigido o nome da chave
+  const registerButton = document.getElementById("registerButton");
+  const password = document.getElementById("password");
+  const email = document.getElementById("email");
+  const username = document.getElementById("username");
+  const date = document.getElementById("birthday");
+  const confirmPassword = document.getElementById("confirmPassword");
+  const eyePassword = document.querySelector(".eyePassword");
+  const eyeConfirmPassword = document.querySelector(".eyeConfirmPassword");
 
-function saveToLocalStorage() {
-  localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
-}
-
-const validation = (userEmail, userPassword) => {
-  if (userEmail.value === "" || userPassword.value === "") {
-    showModalError(modalError, "Preencha todos os campos");
-    return false;
-  } else if (!validationPassword(userPassword)) {
-    showModalError(modalError, "Senha fraca");
-    return false;
-  } else if (!validationEmail(userEmail)) {
-    showModalError(modalError, "Email inválido");
-    return false;
+  function saveToLocalStorage() {
+    localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
   }
-  return true;
-};
 
-const validationPassword = (password) => {
-  const regex =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
-  regex.test(password) ? false : true;
-};
-
-const validationEmail = (email) => {
-  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-  regex.test(email) ? false : true;
-};
-
-function register() {
-  const user = {
-    username: username.value,
-    email: email.value,
-    password: password.value,
-    birthday: date.value,
+  const validation = (userEmail, userPassword) => {
+    if (userEmail.value === "" || userPassword.value === "") {
+      showModalError(modalError, "Preencha todos os campos");
+      return false;
+    } else if (!validationPassword(userPassword)) {
+      showModalError(modalError, "Senha fraca");
+      return false;
+    } else if (!validationEmail(userEmail)) {
+      showModalError(modalError, "Email inválido");
+      return false;
+    }
+    return true;
   };
 
-  registeredUsers.push(user);
-  saveToLocalStorage();
-}
+  const validationPassword = (password) => {
+    const regex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
+    regex.test(password) ? false : true;
+  };
 
-registerButton.addEventListener("click", () => {
-  validation(registeredUsers.email, registeredUsers.password);
-  register();
+  const validationEmail = (email) => {
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+    regex.test(email) ? false : true;
+  };
+
+  function register() {
+    const user = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+      birthday: date.value,
+    };
+
+    registeredUsers.push(user);
+    saveToLocalStorage();
+  }
+
+  registerButton.addEventListener("click", () => {
+    validation(registeredUsers.email, registeredUsers.password);
+    register();
+  });
+
+  eyes(eyePassword, password);
+  eyes(eyeConfirmPassword, confirmPassword);
 });
