a3c4090f80ac87dbcaf9220e1a9aa0fd3d2d70d3
tiago <tiagopinzgraffunder@gmail.com>
2025-01-25 09:18:26 -0300

    refactor: logout apenas no sair

--- public/js/menuLogout.js
+++ public/js/menuLogout.js
@@ -1,5 +1,9 @@
 const menu = document.querySelector(".header__profile--image");
+const logout = document.querySelector(".header__toggle--logout");
 menu.addEventListener("click", () => {
   const menuToggle = document.querySelector(".menu__toggle");
   menuToggle.classList.toggle("active");
+  logout.addEventListener("click", () => {
+    localStorage.removeItem("loggedUser");
+  });
 });
