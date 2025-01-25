85dc78a14c236db4d47b4fed2d8b86838fc319f6
tiago <tiagopinzgraffunder@gmail.com>
2025-01-25 09:01:29 -0300

    refactor: tirando load

--- public/js/login.js
+++ public/js/login.js
@@ -3,37 +3,35 @@ import { eyes } from "./eyes.js";
 import { showModalError } from "./modalError.js";
 import { showModalOk } from "./modalOk.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const userEmail = document.getElementById("userEmail");
-  const userPassword = document.getElementById("userPassword");
-  const eye = document.querySelector(".eye");
-  const loginButton = document.getElementById("loginButton");
-  const modalError = document.getElementById("modalErrorLogin");
-  const modalOk = document.getElementById("modalOkLogin");
-  let ok = false;
+const userEmail = document.getElementById("userEmail");
+const userPassword = document.getElementById("userPassword");
+const eye = document.querySelector(".eye");
+const loginButton = document.getElementById("loginButton");
+const modalError = document.getElementById("modalErrorLogin");
+const modalOk = document.getElementById("modalOkLogin");
+let ok = false;
 
-  function loginUser() {
-    userEmail.value === "" || userPassword.value === ""
-      ? showModalError(modalError, "Preencha todos os campos")
-      : (ok = true);
-    if (ok) {
-      const user = getUser(userEmail.value, userPassword.value);
-      if (user) {
-        showModalOk(modalOk, `${user.username} logado com sucesso...`);
-        setTimeout(() => {
-          window.location.href = "./public/pages/vehicles.html";
-          localStorage.setItem("loggedUser", JSON.stringify(user));
-        }, 1000);
-      } else {
-        showModalError(modalError, "Usuário não encontrado");
-      }
+function loginUser() {
+  userEmail.value === "" || userPassword.value === ""
+    ? showModalError(modalError, "Preencha todos os campos")
+    : (ok = true);
+  if (ok) {
+    const user = getUser(userEmail.value, userPassword.value);
+    if (user) {
+      showModalOk(modalOk, `${user.username} logado com sucesso...`);
+      setTimeout(() => {
+        window.location.href = "./public/pages/vehicles.html";
+        localStorage.setItem("loggedUser", JSON.stringify(user));
+      }, 1000);
+    } else {
+      showModalError(modalError, "Usuário não encontrado");
     }
   }
+}
 
-  loginButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    loginUser();
-  });
-
-  eyes(eye, userPassword);
+loginButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  loginUser();
 });
+
+eyes(eye, userPassword);
