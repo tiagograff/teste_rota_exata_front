4aa3411a140e1dfa2df65d448ec225820cb4b068
tiago <tiagopinzgraffunder@gmail.com>
2025-01-28 18:05:00 -0300

    feat: filtros de propósito de uso, placa e em conjuntos

--- public/js/filters.js
+++ public/js/filters.js
@@ -3,16 +3,19 @@ import { renderTable, renderPagination } from "./tableVehicles.js";
 let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
 let vehicles = loggedUser && loggedUser.vehicles ? loggedUser.vehicles : [];
 let originalVehicles = [...vehicles];
-let filteredVehicles = [...vehicles];
+let filteredVehiclesMarks = [...vehicles];
+let filteredVehiclesPurpose = [...vehicles];
+let filteredVehiclesPlate = [...vehicles];
 
 const rowsPerPage = 7;
 let currentPage = 1;
 
 const filterButtonSearch = document.getElementById("filterButton");
-
 const filterMarks = document.querySelectorAll(
   ".container__filter--options .options input[type='checkbox']"
 );
+const filterPurpose = document.getElementById("purposeOfUse");
+const filterPlateInput = document.getElementById("vehiclesPlate");
 
 function getSelectedMarks() {
   return Array.from(filterMarks)
@@ -22,20 +25,39 @@ function getSelectedMarks() {
 
 function filterVehicles() {
   const selectedMarks = getSelectedMarks();
+  let plateValue = filterPlateInput.value.trim();
+  let purposeValue = filterPurpose.options[filterPurpose.selectedIndex].value;
 
   if (selectedMarks.length > 0) {
-    filteredVehicles = originalVehicles.filter((vehicle) =>
+    filteredVehiclesMarks = originalVehicles.filter((vehicle) =>
       selectedMarks.some((mark) => vehicle.mark.includes(mark))
     );
   } else {
-    filteredVehicles = [...originalVehicles];
+    filteredVehiclesMarks = [...originalVehicles];
+  }
+
+  if (purposeValue.length > 0) {
+    filteredVehiclesPurpose = filteredVehiclesMarks.filter((vehicle) =>
+      vehicle.purpose.includes(purposeValue)
+    );
+  } else {
+    filteredVehiclesPurpose = [...filteredVehiclesMarks];
+  }
+
+  if (plateValue.length > 0) {
+    filteredVehiclesPlate = filteredVehiclesPurpose.filter((vehicle) =>
+      vehicle.plate.includes(plateValue)
+    );
+  } else {
+    filteredVehiclesPlate = [...filteredVehiclesPurpose];
   }
 
   currentPage = 1;
-  renderTable(currentPage, filteredVehicles);
-  renderPagination(filteredVehicles.length, rowsPerPage);
+  renderTable(currentPage, filteredVehiclesPlate);
+  renderPagination(filteredVehiclesPlate.length, rowsPerPage);
 }
 
 filterButtonSearch.addEventListener("click", filterVehicles);
+
 renderTable(currentPage, vehicles);
 renderPagination(vehicles.length, rowsPerPage);
